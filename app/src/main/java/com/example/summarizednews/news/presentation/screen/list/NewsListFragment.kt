c8ed0682ff3908c8667e63a59e2c27794b97d370
ErroredPasta <ForeignServices3132@pm.me>
2023-03-10 20:03:56 +0900

    [Refactor] Deleted BaseFragment and added some extension functions

--- app/src/main/java/com/example/summarizednews/news/presentation/screen/list/NewsListFragment.kt
+++ app/src/main/java/com/example/summarizednews/news/presentation/screen/list/NewsListFragment.kt
@@ -2,31 +2,38 @@ package com.example.summarizednews.news.presentation.screen.list
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.summarizednews.R
-import com.example.summarizednews.core.presentation.BaseFragment
+import com.example.summarizednews.core.presentation.collectLatestWhenStarted
+import com.example.summarizednews.core.presentation.showToast
 import com.example.summarizednews.databinding.FragmentNewsListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NewsListFragment : BaseFragment<FragmentNewsListBinding>() {
+class NewsListFragment : Fragment() {
     private val adapter = NewsListAdapter()
     private val newsListViewModel by viewModels<NewsListViewModel>()
     private val navController by lazy { findNavController() }
+    private var recyclerView: RecyclerView? = null
 
-    override fun inflateBinding(
+    override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): FragmentNewsListBinding = FragmentNewsListBinding.inflate(inflater, container, false).apply {
+    ): View = FragmentNewsListBinding.inflate(inflater, container, false).apply {
         newsListRecyclerView.adapter = adapter
         viewModel = newsListViewModel
         setLifecycleOwner { this@NewsListFragment.viewLifecycleOwner.lifecycle }
         newsListRefreshLayout.setOnRefreshListener { newsListViewModel.fetchNewsList() }
     }.also { binding ->
-        newsListViewModel.state.collectWhenStarted { state ->
+        recyclerView = binding.newsListRecyclerView
+
+        newsListViewModel.state.collectLatestWhenStarted(lifecycleOwner = viewLifecycleOwner) { state ->
             if (state.isLoading) {
                 binding.newsListLoadingShimmer.startShimmer()
             } else {
@@ -51,5 +58,11 @@ class NewsListFragment : BaseFragment<FragmentNewsListBinding>() {
                 newsListViewModel.navigationDone()
             }
         }
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        recyclerView?.adapter = null
+        recyclerView = null
     }
 }
