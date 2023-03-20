fa944b75a147647db95541a3df88a1946794907e
ErroredPasta <ForeignServices3132@pm.me>
2023-03-20 17:49:02 +0900

    [Fix] Fixed ACTIVITY_KTX

--- buildSrc/src/main/java/com/example/buildsrc/AndroidKtx.kt
+++ buildSrc/src/main/java/com/example/buildsrc/AndroidKtx.kt
@@ -1,7 +1,7 @@
 package com.example.buildsrc
 
 private const val ACTIVITY_KTX_VERSION = "1.6.1"
-const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.6.1$ACTIVITY_KTX_VERSION"
+const val ACTIVITY_KTX = "androidx.activity:activity-ktx:$ACTIVITY_KTX_VERSION"
 
 private const val FRAGMENT_KTX_VERSION = "1.5.5"
 const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:$FRAGMENT_KTX_VERSION"
